f9705df73712d0503bbabaff216d165944688c33
rohit <rohitsawant@gmail.com>
2023-11-06 13:00:33 +0530

    changes

--- src/component/purecomponent/PureComponent.jsx
+++ src/component/purecomponent/PureComponent.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import Child from '../purecomponent/Child';
 
-// import { userContext } from '../usecontext/UseContext';
 const PureComponent = () => {
-
-    // let { name } = useContext(userContext)
-    // console.log('name', name)
 
     let [count, setCount] = useState(0);
     let [todo, setTodo] = useState([])
