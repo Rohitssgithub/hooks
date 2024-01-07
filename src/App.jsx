3586832cf4e79ee1f5ed4ff51a7d7a0fab5035ef
rohit <rohitsawant@gmail.com>
2024-01-07 16:14:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,11 @@
 import React from 'react'
-// import UsecallBacks from './component/CallBack/UsecallBack'
-// import PureComponent from './component/purecomponent/PureComponent'
-// import UseMemo from './component/UseMemoHook/UseMemo'
-import { UseContextcom } from './component/usecontext/UseContext'
 import Alldata from './component/AllData/Alldata';
-import Debounce from './component/Debounce'
-import DeBounceing from './component/DeBounceing'
-import Todos from './component/UseReducer'
-import ForwardRef from './component/ForwardRef'
-import UseTransistion from './component/UseTransistion'
-import ChildA from './component/HigherOrderComponenet/ChildA'
-import ChildB from './component/HigherOrderComponenet/ChildB'
 import RegistrationForm from './component/RegistrationForm/RegistrationForm'
-import PureComponent from './component/pages/PureComponent'
-import UseMemo from './component/pages/UseMemo';
-import UseCallBack from './component/pages/UseCallBack';
-import DebounceExample from './component/Debounce';
-import DebounceReact from './component/pages/DebounceReact';
-import DebouncingMain from './component/pages/DebouncingMain';
-import CustomHook from './component/pages/CustomHook';
-import Com from './component/pages/Com';
-import { UseContextFun } from './component/UseContext';
-import ContextChild from './component/ContextChild';
-import ComA from './component/HigherOrderComponent/ComA';
-import ComB from './component/HigherOrderComponent/ComB';
-import Debouncing from './component/Debouncing/Debouncing';
-import Child from './component/purecomponent/Child';
-import UseReducer from './component/usereducer/UseReducer';
-import Promises from './component/Promises';
-import ForWardRefExample from './component/pages/ForWardRefExample';
 const App = () => {
   return (
     <>
-    <ForWardRefExample></ForWardRefExample>
-    <Promises></Promises>
-      {/* <UseReducer></UseReducer> */}
-      {/* <PureComponent /> */}
-
-      {/* <Debouncing></Debouncing> */}
-      {/* <ComA></ComA>
-    <ComB></ComB>
-      <UseContextFun>
-        <ContextChild />
-      </UseContextFun> */}
-      {/* <Com></Com>
-    <CustomHook></CustomHook> */}
-      {/* <DebouncingMain /> */}
-      {/* <DebounceReact/> */}
-      {/* <UseCallBack></UseCallBack> */}
-      {/* <UseMemo></UseMemo> */}
-      {/* <RegistrationForm></RegistrationForm> */}
-      {/* <ChildA></ChildA>
-      <ChildB></ChildB> */}
-      {/* <UseTransistion></UseTransistion> */}
-      {/* <PureComponent /> */}
-      {/* <ForwardRef></ForwardRef>
-      <Todos></Todos>
-      <DeBounceing /> */}
-      {/* <DeBounceing /> */}
-      {/* <Debounce></Debounce> */}
-      {/* <CreatePokemon /> */}
-      {/* <Alldata></Alldata> */}
-      {/* <UseContextcom> */}
-      {/* <UsecallBacks></UsecallBacks>
-        <UseMemo></UseMemo> */}
-      {/* <PureComponent />
-      </UseContextcom> */}
-
-
+     <Alldata></Alldata>
+     <RegistrationForm/>
     </>
   )
 }
